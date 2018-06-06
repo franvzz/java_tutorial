d9645377cc57c1c95f8681fbcb2a5c174c20a50b
Javier <javier@emprendoor.com>
2018-06-05 20:55:42 -0500

    Inicio de Tutorial

---
+++ 2_Variables_y_Operaciones/src/paquete/Main.java
@@ -0,0 +1,30 @@
+package paquete;
+
+class Main {
+	
+	/**
+	 * short - numeros cortos
+	 * int - numeros enteros
+	 * long - numeros enteros largos
+	 * float - numeros flotantes
+	 * double - numeros dobles
+	 * byte - se maneja en bytes
+	 * char - caracteres
+	 * boolean - boleanos, true o false
+	 * string - cadena de texto
+	 * Operadores: + - * / %
+	 * 
+	 * */
+	public static void main(String[] args) {
+		//int variable1, variable2, resultado;
+		//variable1 = 5;
+		//variable2 = 5;
+		//resultado = variable1 * 2;
+		//System.out.println("El resultado es: " + resultado);
+		
+		for(int contador = 0; contador<11; contador++) {
+			System.out.println("El contador es: "+contador);
+		}
+	}
+
+}
---
+++ IntroduccionObjetos/src/paquete/Main.java
@@ -0,0 +1,20 @@
+package paquete;
+import paquete.Pelota;
+import paquete.Carro;
+
+class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Pelota p;
+		//p = new Pelota();
+		//p.patearPelota();
+		
+		CarroBMW c;
+		c = new CarroBMW();
+		c.encender();
+		c.turbo();
+		c.apagar();
+	}
+
+}
